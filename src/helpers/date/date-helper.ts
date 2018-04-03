0df25abbe27c110c6e2cbee01559dad0a7784351
maph maphskiy <maphskiy@gmail.com>
2018-04-03 12:06:30 +0300

    added some methods to date helper

--- src/helpers/date/date-helper.ts
+++ src/helpers/date/date-helper.ts
@@ -7,7 +7,6 @@ export class DateHelper {
         if (!date || !moment(date).isValid) {
           return null
         }
-        let formatted = moment(date);
         return moment(date).format(format);
     };
     public static parseDate (date: any, format?: string) {
@@ -18,5 +17,36 @@ export class DateHelper {
             return null
         }
         return moment(date, format).toISOString();
+    };
+
+    public static dateToString(date: Date, format?: string): string | null {
+        if(!format) {
+            format = "DD.MM.YYYY";
+        }
+        if (!date || !moment(date).isValid) {
+          return null;
+        }
+        let formatted = moment(date);
+        return moment(date).format(format);
+    };
+
+    public static dateToStringISO(date: Date, format?: string): string | null {
+        if(!format) {
+            format = "DD.MM.YYYY";
+        }
+        if (!date || !moment(date).isValid) {
+          return null;
+        }
+        return moment(date, format).toISOString();
+    };
+
+    public static stringToDate(date: string, format?: string): Date | null {
+        if(!format) {
+            format = "DD.MM.YYYY";
+        }
+        if (!date || !moment(date, format).isValid) {
+            return null
+        }
+        return moment(date, format).toDate();
     };
 }
