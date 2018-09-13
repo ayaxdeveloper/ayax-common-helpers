b431b3256d19281ef53726ec21ebb480c29579cf
maph maphskiy <maphskiy@gmail.com>
2018-09-13 18:39:41 +0300

    package update

--- src/helpers/ArraySortHelper.ts
+++ src/helpers/ArraySortHelper.ts
@@ -1,50 +1,74 @@
 export class ArraySortHelper {
-    private _key: string;
-    constructor(key: string) {
-        this._key = key;
+    private _key: string[];
+    constructor(key: string | string[]) {
+        if (Array.isArray(key)) {
+            this._key = key;
+        } else {
+            this._key = [key];
+        }
     }
 
-    static Sort(key: string) {
+    static SortBy(key: string | string[]) {
         return new ArraySortHelper(key);
     }
 
-    public Asc(a: any, b: any) {
-        if (!this._key) {
-            return 0;
-        }
-        if (!a[this._key] || !b[this._key]) {
-            return 0;
-        }
-        if (a[this._key] > b[this._key]) {
-            return 1;
-        }
-        if (a[this._key] < b[this._key]) {
-            return -1;
-        }
-        return 0;
+    public Asc(a: any, b: any) : number {
+        return this.Sort(a, b, true);
     }
 
-    public Desc(a: any, b: any) {
-        if (!this._key) {
-            return 0;
-        }
-        if (!a[this._key] || !b[this._key]) {
-            return 0;
-        }
-        if (a[this._key] < b[this._key]) {
-            return 1;
-        }
-        if (a[this._key] > b[this._key]) {
-            return -1;
-        }
-        return 0;
+    public Desc(a: any, b: any) : number {
+        return this.Sort(a, b, false);
     }
 
     static byOrder(a: any, b: any) {
-        this.Sort("order").Asc(a, b);
+        return this.SortBy("order").Asc(a, b);
     }
     
     static byOrderDesc(a: any, b: any) {
-        this.Sort("order").Desc(a, b);
+        return this.SortBy("order").Desc(a, b);
+    }
+
+    private FormatValue(val: any): any {
+        if (typeof val === "string") {
+            return val.toLocaleLowerCase();
+        }
+        return val;
+    }
+
+    private SortByKey(a: any, b: any, key: string, asc?: boolean) : number {
+        asc = asc ? asc : true;
+        if (!a[key] || !b[key]) {
+            return 0;
+        }
+
+        const aFormatted = this.FormatValue(a[key]);
+        const bFormatted = this.FormatValue(b[key]);
+
+        if (asc) {
+            if (aFormatted > bFormatted) {
+                return 1;
+            }
+            if (aFormatted < bFormatted) {
+                return -1;
+            }
+        } else {
+            if (aFormatted < bFormatted) {
+                return 1;
+            }
+            if (aFormatted > bFormatted) {
+                return -1;
+            }
+        }
+        return 0;
+    }
+
+    private Sort(a: any, b: any, asc?: boolean) : number {
+        this._key.forEach(key => {
+            const sort = this.SortByKey(a, b, key, asc);
+            if (sort !== 0) {
+                return sort;
+            }
+        });
+        return 0;
     }
 }
