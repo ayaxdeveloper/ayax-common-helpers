0f95566fcc5f29fc3f9c30cf253a9d609019e93f
maph maphskiy <maphskiy@gmail.com>
2018-09-13 16:49:00 +0300

    Допилил ArraySortHelper

--- src/helpers/ArraySortHelper.ts
+++ src/helpers/ArraySortHelper.ts
@@ -1,26 +1,50 @@
 export class ArraySortHelper {
-    static byOrder(a: any, b: any) {
-        if (!a.order || !b.order) {
+    private _key: string;
+    constructor(key: string) {
+        this._key = key;
+    }
+
+    static Sort(key: string) {
+        return new ArraySortHelper(key);
+    }
+
+    public Asc(a: any, b: any) {
+        if (!this._key) {
             return 0;
         }
-        if (a.order > b.order) {
+        if (!a[this._key] || !b[this._key]) {
+            return 0;
+        }
+        if (a[this._key] > b[this._key]) {
             return 1;
         }
-        if (a.order < b.order) {
+        if (a[this._key] < b[this._key]) {
             return -1;
         }
         return 0;
     }
-    static byOrderDesc(a: any, b: any) {
-        if (!a.order || !b.order) {
+
+    public Desc(a: any, b: any) {
+        if (!this._key) {
             return 0;
         }
-        if (a.order < b.order) {
+        if (!a[this._key] || !b[this._key]) {
+            return 0;
+        }
+        if (a[this._key] < b[this._key]) {
             return 1;
         }
-        if (a.order > b.order) {
+        if (a[this._key] > b[this._key]) {
             return -1;
         }
         return 0;
+    }
+
+    static byOrder(a: any, b: any) {
+        this.Sort("order").Asc(a, b);
+    }
+    
+    static byOrderDesc(a: any, b: any) {
+        this.Sort("order").Desc(a, b);
     }
 }
